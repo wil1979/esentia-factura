5ea73d100878965df2b29eeb2bfb48b70a67b03c
wil1979 <wilberthcm2009@hotmail.com>
2025-07-27 16:49:07 -0600

    Add files via upload

---
+++ clientes/script.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB8F2QzImoa64IAzbq_rJ6Jx8XHa8UThQA",
+    authDomain: "esentiacreditos.firebaseapp.com",
+    projectId: "esentiacreditos",
+    storageBucket: "esentiacreditos.firebasestorage.app",
+    messagingSenderId: "793663803794",
+    appId: "1:793663803794:web:40685ea7b05f477d64f14b"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
